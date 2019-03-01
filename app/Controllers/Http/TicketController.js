a21feda9e2145f34ec5733cb38919f074391f270
Marcus Mann <mpereirassa@gmail.com>
2019-03-01 14:03:19 -0300

    remove all comments and implement index

--- app/Controllers/Http/TicketController.js
+++ app/Controllers/Http/TicketController.js
@@ -1,85 +1,21 @@
 "use strict";
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Ticket = use("App/Models/Ticket");
 
-/**
- * Resourceful controller for interacting with tickets
- */
 class TicketController {
-  /**
-   * Show a list of all tickets.
-   * GET tickets
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index({ request, response, view }) {}
+  async index({ request }) {
+    const currentPage = request.all();
+    return Ticket.query()
+      .with("user")
+      .paginate(currentPage.page, 10);
+  }
 
-  /**
-   * Render a form to be used for creating a new ticket.
-   * GET tickets/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create({ request, response, view }) {}
-
-  /**
-   * Create/save a new ticket.
-   * POST tickets
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async store({ request, response }) {}
 
-  /**
-   * Display a single ticket.
-   * GET tickets/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show({ params, request, response, view }) {}
 
-  /**
-   * Render a form to update an existing ticket.
-   * GET tickets/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit({ params, request, response, view }) {}
-
-  /**
-   * Update ticket details.
-   * PUT or PATCH tickets/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update({ params, request, response }) {}
 
-  /**
-   * Delete a ticket with id.
-   * DELETE tickets/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async destroy({ params, request, response }) {}
 }
 
