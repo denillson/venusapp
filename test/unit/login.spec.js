f9937b6d04b93d6b1fd4d113697147fe7c5a44ad
Marcus Mann <mpereirassa@gmail.com>
2019-03-01 09:38:51 -0300

    Add + test for login

--- test/unit/login.spec.js
+++ test/unit/login.spec.js
@@ -6,11 +6,11 @@ const User = use("App/Models/User");
 trait("Test/ApiClient");
 trait("DatabaseTransactions");
 
-test("Authentication => Login", async ({ client }) => {
+test("Login User", async ({ client }) => {
   const payload = {
-    username: "spiderman",
-    email: "spiderman@marvel.com",
-    password: "spider123"
+    username: "example",
+    email: "example@example.com",
+    password: "testing"
   };
 
   await User.create({ ...payload });
@@ -25,4 +25,73 @@ test("Authentication => Login", async ({ client }) => {
     refreshToken: null,
     type: "bearer"
   });
+});
+
+test("Username is Null", async ({ client }) => {
+  const payload = {
+    username: null,
+    email: "example@example.com",
+    password: "testing"
+  };
+
+  var response = await client
+    .post("/api/v1/login")
+    .header("accept", "application/json")
+    .send(payload)
+    .end();
+
+  response.assertStatus(400);
+  response.assertError([
+    {
+      message: "o campo username é obrigatório.",
+      field: "username",
+      validation: "required"
+    }
+  ]);
+});
+
+test("Email is Null", async ({ client }) => {
+  const payload = {
+    username: "example",
+    email: null,
+    password: "testing"
+  };
+
+  var response = await client
+    .post("/api/v1/login")
+    .header("accept", "application/json")
+    .send(payload)
+    .end();
+
+  response.assertStatus(400);
+  response.assertError([
+    {
+      message: "o campo e-mail é obrigatório.",
+      field: "email",
+      validation: "required"
+    }
+  ]);
+});
+
+test("Password is Null", async ({ client }) => {
+  const payload = {
+    username: "example",
+    email: "example@example.com",
+    password: null
+  };
+
+  var response = await client
+    .post("/api/v1/login")
+    .header("accept", "application/json")
+    .send(payload)
+    .end();
+
+  response.assertStatus(400);
+  response.assertError([
+    {
+      message: "o campo password é obrigatório.",
+      field: "password",
+      validation: "required"
+    }
+  ]);
 });
