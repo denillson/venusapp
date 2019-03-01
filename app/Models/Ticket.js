011ac0dcb3b2f931ed71a5d529b04191298f3825
Marcus Mann <mpereirassa@gmail.com>
2019-03-01 14:02:05 -0300

    define ticket model , add lifecycle hook for  slug and hidden user_id

--- app/Models/Ticket.js
+++ app/Models/Ticket.js
@@ -3,6 +3,25 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
 
-class Ticket extends Model {}
+class Ticket extends Model {
+  static boot() {
+    super.boot();
+
+    this.addHook("beforeSave", async userInstance => {
+      userInstance.slug = userInstance.title
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/\s/g, "-")
+        .replace(/\./g, "-")
+        .replace(/\,/g, "-");
+    });
+  }
+  static get hidden() {
+    return ["user_id"];
+  }
+  user() {
+    return this.belongsTo("App/Models/User");
+  }
+}
 
 module.exports = Ticket;
